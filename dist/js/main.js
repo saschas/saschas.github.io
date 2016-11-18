d4786d29fa7e513201d3354fd1b093a114de0cdb
saschas <sascha.sigl@googlemail.com>
2016-11-18 09:41:12 +0100

    scrollbar
    Signed-off-by: saschas <sascha.sigl@googlemail.com>

--- dist/js/main.js
+++ dist/js/main.js
@@ -9991,7 +9991,7 @@ CodeMirror.defineMIME("application/typescript", { name: "javascript", typescript
  * http://modernizr.com/download/?-touchevents !*/
 !function(e,n,t){function o(e,n){return typeof e===n}function s(){var e,n,t,s,a,i,r;for(var l in c){if(e=[],n=c[l],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(t=0;t<n.options.aliases.length;t++)e.push(n.options.aliases[t].toLowerCase());for(s=o(n.fn,"function")?n.fn():n.fn,a=0;a<e.length;a++)i=e[a],r=i.split("."),1===r.length?Modernizr[r[0]]=s:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=s),f.push((s?"":"no-")+r.join("-"))}}function a(e){var n=p.className,t=Modernizr._config.classPrefix||"";if(h&&(n=n.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+t+"no-js(\\s|$)");n=n.replace(o,"$1"+t+"js$2")}Modernizr._config.enableClasses&&(n+=" "+t+e.join(" "+t),h?p.className.baseVal=n:p.className=n)}function i(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):h?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}function r(){var e=n.body;return e||(e=i(h?"svg":"body"),e.fake=!0),e}function l(e,t,o,s){var a,l,f,c,d="modernizr",u=i("div"),h=r();if(parseInt(o,10))for(;o--;)f=i("div"),f.id=s?s[o]:d+(o+1),u.appendChild(f);return a=i("style"),a.type="text/css",a.id="s"+d,(h.fake?h:u).appendChild(a),h.appendChild(u),a.styleSheet?a.styleSheet.cssText=e:a.appendChild(n.createTextNode(e)),u.id=d,h.fake&&(h.style.background="",h.style.overflow="hidden",c=p.style.overflow,p.style.overflow="hidden",p.appendChild(h)),l=t(u,e),h.fake?(h.parentNode.removeChild(h),p.style.overflow=c,p.offsetHeight):u.parentNode.removeChild(u),!!l}var f=[],c=[],d={_version:"3.1.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var t=this;setTimeout(function(){n(t[e])},0)},addTest:function(e,n,t){c.push({name:e,fn:n,options:t})},addAsyncTest:function(e){c.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=d,Modernizr=new Modernizr;var u=d._config.usePrefixes?" -webkit- -moz- -o- -ms- ".split(" "):[];d._prefixes=u;var p=n.documentElement,h="svg"===p.nodeName.toLowerCase(),m=d.testStyles=l;Modernizr.addTest("touchevents",function(){var t;if("ontouchstart"in e||e.DocumentTouch&&n instanceof DocumentTouch)t=!0;else{var o=["@media (",u.join("touch-enabled),("),"heartz",")","{#modernizr{top:9px;position:absolute}}"].join("");m(o,function(e){t=9===e.offsetTop})}return t}),s(),a(f),delete d.addTest,delete d.addAsyncTest;for(var v=0;v<Modernizr._q.length;v++)Modernizr._q[v]();e.Modernizr=Modernizr}(window,document);
 //_______________________________________________
-
+var d = document;
 var base_url = 'https://saschas.github.io/';
 var holder = document.getElementById("holder");
 var infoPanel = document.getElementById('infoPanel');
@@ -10061,14 +10061,26 @@ function createIframe(url,src,hash){
       closeDisqus.innerHTML = "close comments";
 
   var aside = document.createElement('aside');
-      aside.classList.add('comments')
+      aside.classList.add('comments');
   var commentsThread = document.createElement('div');
       commentsThread.setAttribute('id', 'disqus_thread');
         iFrameHolder.appendChild(aside);
 
+  //comment Count
+  // var commentCount = document.createElement('span');
+  //     commentCount.classList.add('disqus-comment-count');
+  //     commentCount.setAttribute('data-disqus-url', url);
+
+  //     disqusButton.appendChild(commentCount);
+  // var commentScript = d.createElement('script');
+  //     commentScript.src = "//saschasgithubio.disqus.com/count.js";
+  //     commentScript.setAttribute("id", "dsq-count-scr" );
+  //     (d.head || d.body).appendChild(commentScript);
+
       aside.appendChild(commentsThread);
       aside.appendChild(closeDisqus);
         
+
       //set disqus_options on start iframe
       disqus_config = function () {      
         this.page.url = url;
@@ -10076,6 +10088,7 @@ function createIframe(url,src,hash){
         this.page.title = hash.replace('_', ' ');
       };
 
+//Toggle
       function toggleComments(){
         aside.classList.toggle("open");
         disqusButton.classList.toggle("open");
@@ -10086,12 +10099,14 @@ function createIframe(url,src,hash){
           if(!opt.commentsScriptLoaded){
 
             //disqus
-            var d = document, 
+            
                 s = d.createElement('script');
                 s.src = '//saschasgithubio.disqus.com/embed.js';
                 s.setAttribute('data-timestamp', +new Date());
                 (d.head || d.body).appendChild(s);
                 opt.commentsScriptLoaded = true;
+            
+
           }else{
             DISQUS.reset({
               reload: true,

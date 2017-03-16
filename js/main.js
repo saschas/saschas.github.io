9c287c2faf5ff08039ab0c949250dee17ef0b5ec
saschas <sascha.sigl@googlemail.com>
2017-03-16 18:15:53 +0100

    clean
    Signed-off-by: saschas <sascha.sigl@googlemail.com>

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ var opt = {
 };
 var disqus_config;
 
-console.log('%c v_0.02' , 'font-weight: bold; color: #4caf50');
+console.log('%c v_0.03' , 'font-weight: bold; color: #4caf50');
 
 close_single_experiment.addEventListener('click',function(event){
   event.preventDefault(event);
@@ -53,161 +53,7 @@ getData('data.json',function(d){
   console.log("Error loading page\n");
 });
 
-
-function handler(event) {
-  event.preventDefault(event);
-  event.stopPropagation(event);
-  window.location.hash = this._hash;
-
-  old_frame = createIframe(this.href,this._own[0],this._hash);
-}
-
-function createIframe(url,src,hash){
-
-  console.log(url,src,hash);
-  //url.replace('http', 'https');
-  opt.commentsOpen = false;
-  opt.commentsloaded = false;
-
-
-  var iFrameHolder = document.createElement("div");
-      iFrameHolder._own = src;
-  
-  var closer = document.createElement("button");
-      closer.innerHTML = "view source";
-      closer.classList.add("closer");
-      closer.title = "view source";
-      closer.addEventListener("click",viewSource.bind(iFrameHolder));
-
-  var disqusButton = document.createElement('button');
-      disqusButton.innerHTML = 'comments';
-      disqusButton.title = "comments";
-      disqusButton.classList.add('disqus');
-
-  var closeDisqus = document.createElement('button');
-      closeDisqus.classList.add('close-comments');
-      closeDisqus.innerHTML = "close comments";
-
-  var aside = document.createElement('aside');
-      aside.classList.add('comments');
-  var commentsThread = document.createElement('div');
-      commentsThread.setAttribute('id', 'disqus_thread');
-        iFrameHolder.appendChild(aside);
-
-  //comment Count
-  // var commentCount = document.createElement('span');
-  //     commentCount.classList.add('disqus-comment-count');
-  //     commentCount.setAttribute('data-disqus-url', url);
-
-  //     disqusButton.appendChild(commentCount);
-  // var commentScript = d.createElement('script');
-  //     commentScript.src = "//saschasgithubio.disqus.com/count.js";
-  //     commentScript.setAttribute("id", "dsq-count-scr" );
-  //     (d.head || d.body).appendChild(commentScript);
-
-      aside.appendChild(commentsThread);
-      aside.appendChild(closeDisqus);
-        
-
-      //set disqus_options on start iframe
-      disqus_config = function () {      
-        this.page.url = url;
-        this.page.identifier = hash;  
-        this.page.title = hash.replace('_', ' ');
-      };
-
-//Toggle
-      function toggleComments(){
-        aside.classList.toggle("open");
-        disqusButton.classList.toggle("open");
-
-
-        if(!opt.commentsOpen && !opt.commentsloaded){
-          opt.commentsloaded = true;
-          if(!opt.commentsScriptLoaded){
-
-            //disqus
-            
-                s = d.createElement('script');
-                s.src = '//saschasgithubio.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-                opt.commentsScriptLoaded = true;
-            
-
-          }else{
-            DISQUS.reset({
-              reload: true,
-              config: function () {  
-                this.page.url = url;
-                this.page.identifier = hash;  
-                this.page.title = hash.replace('_', ' ');
-              }
-            });
-          }
-        }
-
-        opt.commentsOpen = !opt.commentsOpen;
-      }
-      //toggle sidebar with comments
-      disqusButton.addEventListener('click',toggleComments);
-      closeDisqus.addEventListener('click',toggleComments);
-
-      iFrameHolder.classList.add("iFrameHolder");
-      iFrameHolder.style.position = "fixed";
-      iFrameHolder.style.top = 0;
-      holder.appendChild(iFrameHolder);
-
-console.log(url);
-  var iFrame = document.createElement('iframe');
-      iFrame.src = url;
-      iFrame.style.background = "#fff";
-      iFrame.setAttribute("id","framer");
-      framer = iFrame;
-    
-      iFrameHolder.appendChild(iFrame);
-      iFrameHolder.appendChild(disqusButton);
-      iFrameHolder.appendChild(closer);
-      iFrame.contentWindow.focus();
-      setFrameSize(iFrameHolder,iFrame);
-
-      if(old_frame != null){
-        closeiFrame(old_frame);
-      }
-
-      return iFrameHolder;
-}
-
-
 //____________________________________________________
-
-
-function viewSource() {
-
-  var sourceView = document.createElement("div");
-      sourceView.classList.add("source");
-      this.appendChild(sourceView);
-  var closeSource = document.createElement("div");
-      closeSource.classList.add("closeSource");
-
-      closeSource.addEventListener("click",closeSourceHandler.bind(sourceView),false);
-      sourceView.appendChild(closeSource);
-  
-
-  getData(this._own,function(d){
-    var myCodeMirror = CodeMirror(sourceView, {
-      value: d,
-      mode:  "javascript",
-      theme: "monokai",
-      readOnly : true,
-      lineNumbers: true
-    });
-    myCodeMirror.setSize(sourceView.offsetWidth, sourceView.offsetHeight);
-    myCodeMirror.refresh();
-  },function(){
-    console.log("Error loading page\n");
-  });  
-}
 
 
 function closeSourceHandler(){
@@ -231,9 +77,6 @@ function closeiFrame(frame) {
 function handleData(d){
 
   opt.data = d.data;
-
-  console.log(opt.data);
-
   opt.data.forEach(function(art,index){
 
     var article = document.createElement('article');
@@ -247,7 +90,6 @@ function handleData(d){
           event.preventDefault(event);
           singleExperiment.classList.add('open');
 
-          console.log(base_url + art.link);
           singleExperimentFrame.setAttribute('src',base_url + art.link );
         });
 
@@ -272,111 +114,5 @@ if(art.content != ""){
     holder.appendChild(article);
 
   });
-/*
-  var dataHolder = document.createElement("div");
-      dataHolder.classList.add("dataHolder");
 
-  opt.data.forEach(function(d,index) {
-    var article = document.createElement("div");
-        article.classList.add("single");
-        article.title = d.title;
-
-
-    var contHolder = document.createElement("div");
-        contHolder.classList.add("content");
-
-    var linkIMG = document.createElement("a");
-        linkIMG.setAttribute("href",d.link);
-        linkIMG.classList.add("linkIMG");
-        linkIMG._own = d.jsSRC;
-
-    var link = document.createElement("a");
-        link.classList.add("link");
-        link.innerHTML = d.title;
-        link.setAttribute("href",base_url + d.link);
-        link._own = d.jsSRC;
-        
-    var paragraph = document.createElement("p");
-        paragraph.innerHTML = d.content;
-
-    var title = document.createElement("h2");
-        
-    var img = new Image();
-        img.src = d.img ;
-
-    linkIMG.appendChild(img);
-    title.appendChild(link);
-
-    contHolder.appendChild(title);
-    contHolder.appendChild(paragraph);
-
-    article.appendChild(linkIMG);
-    article.appendChild(contHolder);
-      
-      d.link = link;
-      d.linkIMG = linkIMG;
-      d.hash = d.title.split(' ').join('_');
-      dataHolder.appendChild(article);
-      linkIMG._hash = d.hash;
-      link._hash = d.hash;
-
-
-
-  });
-
-  //var hash = window.location.hash.replace('#','');
-  var hashIndex = 0;
-  // opt.data.forEach(function(d,index){
-  //   if(hash == d.hash){     
-  //     hashIndex = index;
-  //   }
-  // });
-
-  console.log(d.data[hashIndex].link,d.data[hashIndex].jsSRC,d.data[hashIndex].hash);
-  old_frame = createIframe(d.data[hashIndex].link,d.data[hashIndex].jsSRC,d.data[hashIndex].hash);
-
-  infoPanel.appendChild(dataHolder);
-
-  */
 }
-/*
-function setFrameLinks(){
-  if(window.innerWidth > 800){
-    opt.size = "large";
-  }else{
-    opt.size = "small";
-  }
-
-    opt.data.forEach(function(d,index){
-      if(opt.size === "small"){
-        d.link.removeEventListener('click',handler,false);
-        d.linkIMG.removeEventListener('click',handler,false);
-        d.link.setAttribute("target","_blank");
-        d.linkIMG.setAttribute("target","_blank");
-      }else{
-        d.link.addEventListener('click',handler,false);
-        d.linkIMG.addEventListener('click',handler,false);
-        d.link.setAttribute("target","_self");
-        d.linkIMG.setAttribute("target","_self");
-      }
-    });
- }
-
-
-function setFrameSize(holder,f){
-  setFrameLinks();
-  holder.style.width = window.innerWidth - infoPanel.offsetWidth + "px";
-  holder.style.height = window.innerHeight + "px";
-  f.style.width = window.innerWidth - infoPanel.offsetWidth + "px";
-  f.style.height = window.innerHeight + "px";
-}
-
-window.onresize = function(){
-  if(framer != null){
-    setFrameSize(old_frame,framer);
-  }else{
-    setFrameSize(old_frame,document.getElementById('framer'));
-  }
-}
-
-*/
